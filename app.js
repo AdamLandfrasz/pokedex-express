566bf9c9cded2eb895a9843226fb625c44af1733
Landfrasz Ádám <adam.landfrasz@gmail.com>
2020-04-27 12:10:10 +0200

    cors...

--- app.js
+++ app.js
@@ -34,8 +34,16 @@ app.use(logger);
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use("/pokedex/api/caught-pokemon", cors(), pokemonRoute);
-app.use("/pokedex/api/auth", cors(), authRoute);
+app.use(
+  "/pokedex/api/caught-pokemon",
+  cors({ credentials: true, origin: "https://pokedex-master.netlify.app" }),
+  pokemonRoute
+);
+app.use(
+  "/pokedex/api/auth",
+  cors({ credentials: true, origin: "https://pokedex-master.netlify.app" }),
+  authRoute
+);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () =>
