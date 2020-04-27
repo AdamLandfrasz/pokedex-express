acb98d4085a5b2e11f1ec3dec81b4f24073e86db
Landfrasz Ádám <adam.landfrasz@gmail.com>
2020-04-27 14:26:00 +0200

    cookie

--- middleware/withAuth.js
+++ middleware/withAuth.js
@@ -3,6 +3,8 @@ const dotenv = require("dotenv");
 dotenv.config();
 
 const withAuth = (req, res, next) => {
+  console.log(req.cookies);
+
   const token = req.cookies["auth-token"];
   if (!token) return res.send("Access Denied");
 
